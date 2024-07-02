5a7b90261ab059fd0253f0a45bfffba89f8845ea
Figsups <56519521+sakethsatti@users.noreply.github.com>
2024-07-02 03:19:37 +0000

    edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,13 @@
 	$: innerHeight = 0
 </script>
 
+<title>Figsups</title>
+
 <svelte:window bind:innerWidth bind:outerWidth bind:innerHeight bind:outerHeight />
 
 <Navbar />
 <div class="flex h-dvh w-dvh justify-center items-center bg-gradient-to-r from-purple-700 to-indigo-700">
-    <img class="border-8 border-yellow-200 object-cover rounded-full h-1/2 w-auto" alt="Profile: The Portrait by Magritte" src={logo} />
+    <img class="border-8 border-yellow-200 object-cover rounded-full h-96 w-96" alt="Profile: The Portrait by Magritte" src={logo} />
     <div class="ml-10">
         <h1 class="text-8xl font-bold text-sky-300 mb-10 font-dm-sans">Figsups</h1>
         <p class="text-2xl text-white font-dm-sans">Hello! I'm <strong class="text-yellow-400">Saketh</strong>, a Junior at High Technology High School.
